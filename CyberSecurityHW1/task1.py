6e3d711c177b636afb06be43086e8e77248f4889
sfaryadi <42820107+sfaryadi@users.noreply.github.com>
2021-02-18 17:25:27 -0500

    task 1 and 2 done

--- CyberSecurityHW1/task1.py
+++ CyberSecurityHW1/task1.py
@@ -15,15 +15,14 @@ username = input("Please enter a username: ")
 password = getpass.getpass("Enter Password")
 
 length = len(password)
-entropy = math.log((pow(94,length)),2)
+entropy = math.log((pow(95,length)),2)
 
 print("The entropy of that password is:")
 print(entropy)
 
-#I used 94 as the number of possible symbols because I wanted to account for the fact that the password could use any combination of characters
 
 salt = os.urandom(32);
-encrypted = hashlib.pbkdf2_hmac("sha256", b"password", b"salt", 100000)
+encrypted = hashlib.pbkdf2_hmac("sha256", "password".encode("utf-8"), salt, 100000)
 encrypted = encrypted.hex()
 
 file.write("\n" + username + ": " + encrypted)
--- CyberSecurityHW1/task2.py
+++ CyberSecurityHW1/task2.py
@@ -3,6 +3,43 @@ Created on Feb 13, 2021
 
 @author: Stephen
 '''
+import string
+import itertools
+import hashlib
+import os
+import math
+import time
+
+starttime = time.clock()
+
 file = open("passwords.txt", "r")
+lines = file.readlines()
+    
+characters = string.printable[0: 95] 
 
+def brutehack(correcthash):    
+    
+    for i in range(95):
+        combos = list(itertools.combinations(characters, i))
+        for i in range(len(combos)):
+            password =    "".join(combos[i])
+            
+            salt = os.urandom(32);
+            encrypted = hashlib.pbkdf2_hmac("sha256", "password".encode("utf-8"), salt, 100000)
+            encrypted = encrypted.hex()
+            if encrypted==correcthash:
+                print("The password was " + password)
+                
+                length = len(password)
+                entropy = math.log((pow(95,length)),2)
+                print("The entropy was " + entropy)
+                elapsed = time.clock() - starttime
+                print("Time taken to hack the password was " + elapsed)
 
+for i in range(len(lines)):
+    line = lines[i].split(": ")
+    username = line[0]
+    hashed = line[1]
+    
+    brutehack(hashed)
+    
